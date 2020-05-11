8f191f9c96cfad083d3ef32a4dcba91abdd813ff
zeidfazazi <vincedzeid@gmail.com>
2020-05-11 16:39:28 +0200

    Première version de la simulation sous forme de lignes de textes

--- src/monFichierPython.py
+++ src/monFichierPython.py
@@ -2,8 +2,9 @@
 import pyplot as plt
 # Pour modéliser la propagation de rumeurs sur un réseau social, on considère un monde composé d'individus réceptives à la rumeurs, dites S.
 # Pour créer ce monde, on choisit d'utiliser une fonction reseau_social() qui prend en paramètre un entier taille et qui renvoit une liste de liste de 0 afin de créer un monde spatial par la suite(à voir)
-# Le chiffre 0 correspond à une personne N, tous les individus sont de type N au tout début
+# Le chiffre 1 correspond à une personne S, tous les individus sont de type S au tout début
 
+#Quand t=0, S = B, A = I = R = 0
 def reseau_social(taille):
     """int->list[int]
     retourne une liste représentant un réseau social et ses internautes."""
@@ -15,11 +16,34 @@ def reseau_social(taille):
         Lbis = []
         #j : int
         for j in range(0,taille):
-            Lbis.append(1)
+            if(i==0 and j==0):
+                Lbis.append(3)
+            else:
+                Lbis.append(1)
         L.append(Lbis)
     return L
 
-def partage()
+def comptage(world,categorie_individus):
+    if(categorie_individus==-1):
+        #res = int
+        res = 0
+        #i = int
+        for i in world:
+            #j:int
+            for j in i:
+                res = res + 1
+        return res
+    else:
+        #res = int
+        res = 0
+        #i = int
+        for i in world:
+            #j:int
+            for j in i:
+                if j==categorie_individus:
+                    res = res + 1
+        return res
+
 #Visulisation du réseau social
 def plot_world(world):
     A = world
@@ -30,11 +54,11 @@ def plot_world(world):
 
 #Application des formules et définitions des termes, les valeurs de S,A ,I et R varient avec le temps; Teta1, Teta2, Gamma, Phi, B et Micro sont stables tout le long de la simulation
     #S = individu Susceptible, confronté à la rumeur, représenté par 1 dans le réseau social
-def nbre_individu_S_1(B,Sigma,I,Micro,S):
-    return B-Sigma*S*I-Micro*S
+def nbre_individu_S_1(B,S,I,Micro,Phi):
+    return B-Phi*S*I-Micro*S
 
     #A = Individu croyant/ne partangeant pas la rumeur, représenté par 2 dans le réseau social
-def nbre_individu_A_2(Teta1,gamma,phi,micro,h,S,A):
+def nbre_individu_A_2(Teta1,gamma,phi,micro,h,S,A,I):
     return Teta1*phi*S*I - gamma*A + h - micro*A
     #I = Individu croyant/partageant la rumeur, représenté par un 3 dans le réseau social
 def nbre_individu_I_3(Teta2,gamma,phi,micro,h,S,I):
@@ -45,5 +69,41 @@ def nbre_individu_R_4(Teta1,Teta2,gamma,phi,micro,S,A,I,R):
     #Fonction qui determine le nombre d'individus devant garder le silence
 def fonction_h(c,d,I):
     return c*I/(d+I)
+#On considère une rumeur politique, très relayée dans les réseaux sociaux selon nos recherches et expériences personnelles
 
-plot_world(reseau_social(9))
+Phi = 0.4
+Micro = 0.5
+Gamma = 0.3
+Teta1 = 0.2
+Teta2 =0.7
+d= 1
+
+def actualisation_reseau(c):
+    S = 35
+    A = 0
+    I = 1
+    R = 0
+    print("Début de la simulation")
+    print("")
+    for t in range(0,5):
+        if(t==0):
+            print("A t=0, S=35, A=0, I=1, R=0, ")
+            print(" ")
+        else:
+            print("A t =" + str(t))
+            S_mem = S+ nbre_individu_S_1(36,S,I,0.5,0.4)
+            print("Nombre de S =" + str(S_mem))
+            A_mem = A + nbre_individu_A_2(0.2,0.3,0.4,0.5,fonction_h(c,I,1),S,A,I)
+            print("Nombre de A=" + str(A_mem))
+            I_mem = I + nbre_individu_I_3(0.7,0.3,0.4,0.5,fonction_h(c,I,1),S,I)
+            print("Nombre de I="+ str(I_mem))
+            R_mem = R + nbre_individu_R_4(0.2,0.7,0.3,0.4,0.5,S,A,I,R)
+            print("Nombre de R=" + str(R_mem))
+            print("")
+            S = S_mem
+            A = A_mem
+            I = I_mem
+            R = R_mem
+    print("Fin de simulation")
+
+actualisation_reseau(141)
